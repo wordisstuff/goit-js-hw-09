48864d0a26c2f124489bdab031ccf77b5dc0c8ae
wordisstuff <wordisstuff@gmail.com>
2023-10-29 12:30:59 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stopInterval = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+start.addEventListener('click', (evnt) => {
+    evnt.preventDefault();
+    start.disabled = true;
+    stopInterval.disabled = false;
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+});
+
+stopInterval.addEventListener('click', (evnt) => {
+    evnt.preventDefault();
+    start.disabled = false;
+    stopInterval.disabled = true;
+    clearInterval(intervalId);
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
