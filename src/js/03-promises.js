503c2ea732f7d72efc636c3afea249caccc5fe61
wordisstuff <wordisstuff@gmail.com>
2023-10-30 18:25:09 +0200

    task3 v2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -23,9 +23,9 @@ function createPromise(position, delay) {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
       } else {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
       }
     }, delay);
   });
